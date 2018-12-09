29f59349f1e6e81c8e31fdb9d0f1c618c8dc5bdf
Rob <robmoore121@gmail.com>
2018-12-09 16:34:45 +0000

    Solved day 9

--- 9/solution.py
+++ 9/solution.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -7,18 +9,93 @@ class Node:
     def __repr__(self):
         return "<" + str(self.value) + ">"
 
-def link(n1, n2):
-    n1.next = n2
-    n2.prev = n1
-    return n1, n2
+    def fromZero(self):
+        node = self
+        while node.value != 0:
+            node = node.next
 
-def toMarbles(l):
+        result = [node]
+        node = node.next
+        while node.value != 0:
+            result.append(node)
+            node = node.next
+
+        return result
+
+    def take(self, n):
+        result = []
+        node = self
+        while len(result) < n:
+            result.append(node)
+            node = node.next
+
+        return result
+
+    def linkTo(self, n):
+        self.next = n
+        n.prev = self
+        return self, n
+
+    def place(self, v):
+        n = Node(v)
+        n.next = self.next.next
+        n.prev = self.next
+        self.next.next.prev = n
+        self.next.next = n
+        return n
+
+    def moveSevenBack(self):
+        return self.prev.prev.prev.prev.prev.prev.prev # lol
+
+    def remove(self):
+        self.prev.next = self.next
+        self.next.prev = self.prev
+        return self.next
+
+
+def toDoublyLinkedCircularList(l):
     nodes = [Node(l[0])]
     for i in range(1, len(l)):
         newNode = Node(l[i])
-        link(nodes[i - 1], newNode)
+        nodes[i - 1].linkTo(newNode)
         nodes.append(newNode)
 
-    link(nodes[len(nodes) - 1], nodes[0])
+    nodes[len(nodes) - 1].linkTo(nodes[0])
 
-    return nodes
+    return nodes[0]
+
+def play(r, p):
+    m = toDoublyLinkedCircularList([0])
+    currentMarble = 1
+    scores = {}
+    for i in range(p):
+        scores[i] = 0
+
+    currentPlayer = 0
+    while currentMarble <= r:
+        if 0 != currentMarble % 23:
+            m = m.place(currentMarble)
+            currentMarble += 1
+        else:
+            m = m.moveSevenBack()
+            v = m.value
+            m = m.remove()
+            scores[currentPlayer] += currentMarble + v
+
+            currentMarble += 1
+            m = m.place(currentMarble)
+            currentPlayer = (currentPlayer + 1) % p
+            currentMarble += 1
+
+        currentPlayer = (currentPlayer + 1) % p
+
+    return m, scores
+
+def highScore(p, lastMarbleValue):
+    m, scores = play(lastMarbleValue, p)
+    return max(scores.values())
+
+print("The answer to part 1 is " + str(highScore(439, 71307)))
+print("The answer to part 2 is " + str(highScore(439, 71307 * 100)))
+
+exit(0)
