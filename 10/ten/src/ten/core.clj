02d8ef862fd4c19e0612998598b214ff1f217c32
romo <robert.moore@zuhlke.com>
2018-12-10 17:39:31 +0000

    Started to write print-stars

--- 10/ten/src/ten/core.clj
+++ 10/ten/src/ten/core.clj
@@ -5,6 +5,7 @@
 
 (def test-input (str/split (slurp "input/test.txt") #"\n") )
 (def real-input (str/split (slurp "input/input.txt") #"\n") )
+(def input test-input)
 
 (defn drop-edges [s]
   (subs s 1 (- (count s) 1)))
@@ -21,6 +22,42 @@
 (defn parse-stars [input]
   (map parse-line input))
 
+(defn get-positions [stars]
+  (map first stars))
+
+(defn get-velocities [stars]
+  (map second stars))
+
+(defn get-bounds [stars]
+  (let [
+    positions (get-positions stars)
+    max-x (apply max (map first positions))
+    min-x (apply min (map first positions))
+    max-y (apply max (map second positions))
+    min-y (apply min (map second positions))
+  ]
+  (list max-x min-x max-y min-y)))
+
+(defn make-positions-all-positive [stars]
+  (let [
+    bounds (get-bounds stars)
+    x-move (- (second bounds))
+    y-move (- (nth bounds 3))
+    ;; Translates a position (x y) such that it is sure to be positive
+    make-positive #(list (+ x-move (first %)) (+ y-move (second %)))
+    positions (get-positions stars)
+  ]
+  (map make-positive positions)))
+
+(defn print-stars [stars]
+  (let [
+    positions (make-positions-all-positive stars)
+    bounds (get-bounds positions)
+    width (first bounds)
+    height (nth bounds 2)
+  ]
+  5))
+
 (defn -main []
-  (println (count real-input))
-  (println (take 5 (parse-stars real-input)) ))
+  (println (count input))
+  (println (print-stars (parse-stars input))   ))
