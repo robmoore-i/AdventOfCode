37120101d4fb1d73b1fa981b8d60fde005019950
romo <robert.moore@zuhlke.com>
2018-12-10 15:36:43 +0000

    can parse the position and velocity of a star from a line

--- 10/ten/src/ten/core.clj
+++ 10/ten/src/ten/core.clj
@@ -3,8 +3,21 @@
   (:require [clojure.java.io :as io] )
   (:require [clojure.string :as str] ))
 
-(def input (str/split (slurp "input/test.txt") #"\n") )
+(def test-input (str/split (slurp "input/test.txt") #"\n") )
+(def real-input (str/split (slurp "input/input.txt") #"\n") )
+
+(defn drop-edges [s]
+  (let [l (count s)]
+  (subs s 1 (- l 1))))  
+
+(defn parse-line [line]
+  (let [
+    split-line (str/split line #"=")
+    position (map read-string (map str/trim (str/split (apply str (drop 2 (first (str/split (nth split-line 1) #">")))) #", ")))
+    velocity (map read-string (map str/trim (str/split (drop-edges (second (str/split line #" velocity="))) #", ")))
+  ]
+  (cons position [velocity]) ))
 
 (defn -main []
-  (println (count input))
-  (println (nth input 0)) )
+  (println (count real-input))
+  (println (parse-line (nth real-input 2))) )
