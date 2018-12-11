05d09e6fad1bd59ef1fafa51e13db5ce9423b6b6
romo <robert.moore@zuhlke.com>
2018-12-11 11:40:40 +0000

    can score a 3x3 kernel

--- 11/eleven/src/eleven/core.clj
+++ 11/eleven/src/eleven/core.clj
@@ -21,15 +21,34 @@
   ]
   power-level))
 
+(defn matrix [l]
+  (apply vector (map (partial apply vector) l)))
+
+(defn vector-partition [n l]
+  (let [list-partitioned (partition n l)]
+  (matrix list-partitioned)))
+
 (defn build-grid [grid-serial-number side-length]
   (let [
     coordinate-range (range 1 (+ side-length 1))
     grid (combo/cartesian-product coordinate-range coordinate-range)
     power-level (partial apply (partial cell-power-level grid-serial-number))
     cell-power-levels (map power-level grid)
-    power-annotated-grid (partition side-length cell-power-levels)
+    vector-power-grid (vector-partition side-length cell-power-levels)
   ]
-  power-annotated-grid ))
+  vector-power-grid ))
+
+;; Where [x y] marks the top left corner of the 3x3 kernel.
+(defn score-kernel [grid x y]
+  (let [
+    x-coordinates (range x (+ x 3))
+    y-coordinates (range y (+ y 3))
+    indices (matrix (combo/cartesian-product x-coordinates y-coordinates))
+    power-values (map (partial get-in grid) indices)
+    score (reduce + power-values)
+  ]
+  score))
 
 (defn -main []
-  (println (build-grid 42 5) ))
+  (println (build-grid 42 5) )
+  (println (score-kernel (build-grid 42 5) 0 0) ) )
