544d12451ca6d113752c43d011962cb24c1e4dd8
romo <robert.moore@zuhlke.com>
2018-12-12 13:49:22 +0000

    Can find max scoring kernel - but its too slow to be used as a proper solution

--- 11/eleven/src/eleven/core.clj
+++ 11/eleven/src/eleven/core.clj
@@ -39,13 +39,13 @@
     cell-power-levels (map power-level grid)
     vector-power-grid (vector-partition side-length cell-power-levels)
   ]
-  vector-power-grid ))
+  vector-power-grid))
 
 ;; Where [x y] marks the top left corner of the 3x3 kernel in 1-based coordinates.
-(defn score-kernel [grid x y]
+(defn score-kernel [kernel-size grid x y]
   (let [
-    x-coordinates (range (- x 1) (+ x 2))
-    y-coordinates (range (- y 1) (+ y 2))
+    x-coordinates (range (- x 1) (+ x (- kernel-size 1)))
+    y-coordinates (range (- y 1) (+ y (- kernel-size 1)))
     indices (matrix (combo/cartesian-product x-coordinates y-coordinates))
     power-values (map (partial get-in grid) indices)
     score (reduce + power-values)
@@ -55,21 +55,50 @@
 (defn max-index [vec]
   (first (apply max-key second (map-indexed vector vec))))
 
-(defn max-kernel-score [grid]
+(defn max-kernel-score [grid kernel-size]
   (let [
-    coordinate-range (range 1 (- (count grid) 1))
+    coordinate-range (range 1 (- (count grid) (- kernel-size 2)))
     kernel-markers (matrix (self-cross coordinate-range))
-    kernel-scores (map (partial apply (partial score-kernel grid)) kernel-markers)
+    kernel-scores (map (partial apply (partial score-kernel kernel-size grid)) kernel-markers)
     max-kernel-score-idx (max-index kernel-scores)
     max-kernel-score-markers (nth kernel-markers max-kernel-score-idx)
     score (nth kernel-scores max-kernel-score-idx)
   ]
   (cons score max-kernel-score-markers)))
 
-(defn max-grid-score [grid-serial-number side-legth]
-  (max-kernel-score (build-grid grid-serial-number side-legth)))
+(defn max-grid-score [grid-serial-number side-legth kernel-size]
+  (max-kernel-score (build-grid grid-serial-number side-legth) kernel-size))
 
-(def answerp1 (max-grid-score 3031 300))
+(defn append [x coll]
+  (apply conj (list x) (reverse coll)))
+
+(defn max-scoring-kernel-iteration [grid current-max-score kernel-sizes]
+  (let [
+    candidate-kernel-size (first kernel-sizes)
+
+    kernel-score (max-kernel-score grid candidate-kernel-size)
+    score-discriminator (int (Math/sqrt (/ (first kernel-score) 4))) ;; NB. 'int always rounds down
+
+    remaining-kernel-sizes (filter #(> % score-discriminator) (drop 1 kernel-sizes))
+    exceeds-current-max-score (> (first kernel-score) (first current-max-score))
+    new-max-score (if exceeds-current-max-score (append candidate-kernel-size kernel-score) current-max-score)
+  ]
+  (list new-max-score remaining-kernel-sizes)))
+
+(defn max-scoring-kernel [grid-serial-number side-length max-kernel-size]
+  (let [
+    grid (build-grid grid-serial-number side-length)
+    kernel-sizes (reverse (range 3 (+ max-kernel-size 1)))
+    find-max-score (partial max-scoring-kernel-iteration grid)
+
+    iter-one (find-max-score '(0 1 1 0) kernel-sizes)
+    iter #(if (= 0 (count (second %))) % (apply find-max-score %))
+    n-iterations (- (count kernel-sizes) 5)
+    final-result ((apply comp (repeat n-iterations iter)) iter-one)
+  ]
+  final-result))
+
+(def answerp1 (max-grid-score 3031 300 3))
 
 (defn -main []
-  (println  answerp1))
+  (println (max-scoring-kernel 3031 300 5)))
--- 11/eleven/test/eleven/core_test.clj
+++ 11/eleven/test/eleven/core_test.clj
@@ -11,11 +11,19 @@
 
 (deftest can-score-a-kernel
   (testing "Can score a kernel"
-    (is (= 30 (score-kernel (build-grid 42 100) 21 61)))
-    (is (= 29 (score-kernel (build-grid 18 50) 33 45)))))
+    (is (= 30 (score-kernel 3 (build-grid 42 100) 21 61)))
+    (is (= 29 (score-kernel 3 (build-grid 18 50) 33 45)))))
 
 (deftest can-get-max-grid-score-and-position
   (testing "Can find the maximum scoring kernel and its position among all grid kernels"
-    (is (= '(30 21 61) (max-grid-score 42 65)))
-    (is (= '(29 33 45) (max-grid-score 18 50)))
-    (is (= '(30 21 76) (max-grid-score 3031 300)))))
+    (is (= '(30 21 61) (max-grid-score 42 65 3)))
+    (is (= '(29 33 45) (max-grid-score 18 50 3)))
+    (is (= '(30 21 76) (max-grid-score 3031 300 3)))))
+
+(deftest can-get-max-grid-score-and-position-for-full-kernel
+  (testing "Can find the kernel score where kernel size equals side length"
+    (is (= '(-47490 1 1) (max-grid-score 3031 300 300)))))
+
+(deftest can-get-max-scoring-kernel-size
+  (testing "Can find the kernel size where its score is maximised"
+    (is (= (list (list 59 241 119 5) (list 4)) (max-scoring-kernel 3031 300 5)))))
